e8d23f051e633530efff3f463abec88541d62d6f
Satya-macmini <satyap@calipsoclient.com>
2014-01-28 10:46:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '7c3acabaad36d7608b3d8e58c2009a552818d6d9a046432b005dd81c59624b911e7eebbdb2c8aa227d66419580fd00869f7ddd4382c83321b4b5b5f148ce8ca6'
